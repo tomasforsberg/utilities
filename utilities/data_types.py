4c84a5f4feafab217d24ab0b3b8ca20d82f9bbee
tomasforsberg <63699929+tomasforsberg@users.noreply.github.com>
2020-05-07 07:18:58 +0200

    dtype_summarizer

--- utilities/data_types.py
+++ utilities/data_types.py
@@ -1,11 +1,21 @@
-
-def summarize_dtypes(df):
-    dtypes_dict = {t.__str__(): [] for t in df.dtypes}
-    for col, t in df.dtypes.items():
-        dtypes_dict[t.__str__()].append(col)
+class dtype_summarizer:
+    def __init__(self, df):
+        self.d = {t.__str__(): {} for t in df.dtypes}
+        for col, t in df.dtypes.items():
+            self.d[t.__str__()][col] = df[col].unique()[:3]
+        self.dtypes = list(self.d.keys())
+        print('Available dtypes:', self.dtypes)
     
-    for t in dtypes_dict:
+    def __str__(self):
+        for t in self.d:
+            print('|---------------------------------------')
+            print('|', t, '\n|')
+            for col in self.d[t]:
+                print('|    ', col, ', sample: ', self.d[t][col], '\n|')
+    
+    def print_type(self, t):
         print('|---------------------------------------')
         print('|', t, '\n|')
-        for col in dtypes_dict[t]:
-            print('|    ', col, ', sample: ', df[col].unique()[:3], '\n|')
+        for col in self.d[t]:
+            print('|    ', col, ', sample: ', self.d[t][col], '\n|')
+
